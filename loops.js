652a676a74d424800a005810c2f4ddd2ef5aeaf9
Pavan raju <pavankumarraju7@gmail.com>
2022-08-17 15:57:20 +0530

    for while lop

---
+++ loops.js
@@ -0,0 +1,24 @@
+//while loop program
+
+var text = "";
+var i = 0;
+while (i < 10) {                     //while loop condition
+  text += "<br>The number is "; + i;
+  i++;                              //increment operator...
+}
+console.log(text);              //final outoput
+
+
+//for loop 
+
+
+var text = "";
+
+for (var i = 0; i < 5; i++)            //for condition
+
+
+{
+  text += "The number is " + i + "<br>";            
+}
+
+console.log(text);                       //final output
